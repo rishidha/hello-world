2599aa0005692e763db90225ae1230c1e1ed7b37
rishidha <57841979+rishidha@users.noreply.github.com>
2020-06-10 21:28:38 +0530

    Update samplegroovy.groovy

--- samplegroovy.groovy
+++ samplegroovy.groovy
@@ -1,7 +1,7 @@
 job('First groovy script job') {
 	description("First maven groovy script job")
 	scm {
-		git("https://github.com/rishidha/hello-world.git", master)
+		git("https://github.com/rishidha/hello-world.git", 'master')
 	}
 	triggers {
 		scm('* * * * *')
